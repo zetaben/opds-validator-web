0c376d563dd15c4a8b4967b95b27cb9ab2e5a9ed
Benoit Larroque <zeta.ben@gmail.com>
2011-11-15 14:47:48 +0100

    Add direct input validation

--- src/com/feedbooks/opds/webvalidator/OPDSValidatorWebServlet.java
+++ src/com/feedbooks/opds/webvalidator/OPDSValidatorWebServlet.java
@@ -7,7 +7,10 @@ import java.net.URL;
 
 import javax.servlet.http.*;
 
+import org.apache.tools.ant.taskdefs.Sleep;
 import org.json.JSONString;
+
+import sun.awt.windows.ThemeReader;
 
 import com.feedbooks.opds.*;
 import com.google.appengine.api.urlfetch.HTTPMethod;
@@ -20,44 +23,75 @@ import static com.google.appengine.api.urlfetch.FetchOptions.Builder.*;
 
 @SuppressWarnings("serial")
 public class OPDSValidatorWebServlet extends HttpServlet {
+
+	private void handleValidation(HttpServletRequest req,
+			HttpServletResponse resp, String content, String name,
+			String opds_version) throws IOException {
+		Validator v = new Validator();
+
+		v.SetOPDSVersion(opds_version);
+		StringWriter validation_results = new StringWriter();
+		ErrorHandlerImpl eh = new JSONErrorHandlerImpl(validation_results);
+		v.SetErrorHandler(eh);
+
+		String[] args = { content };
+		String[] names = { name };
+		boolean hadErr = v.validate(args, names);
+
+		if (hadErr) {
+			resp.setStatus(409);
+		}
+		eh.close();
+		// resp.getOutputStream();
+		BufferedWriter w = new BufferedWriter(resp.getWriter());
+		w.append("{ \"results\": ");
+		w.append(validation_results.getBuffer());
+		w.append(", \"source\": ");
+		w.append(JSONObject.quote(content));
+		w.append("}");
+
+		w.close();
+
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("application/json");
 		String uri = req.getParameter("uri");
 		if (uri != null && !uri.equals("") && uri.startsWith("http")) {
-			Validator v = new Validator();
 			String opds_version = "1.0";
 			String ve = req.getParameter("opds_version");
 			if (ve != null && !ve.equals("")) {
 				opds_version = ve;
 			}
-			v.SetOPDSVersion(opds_version);
-			StringWriter validation_results=new StringWriter();
-			ErrorHandlerImpl eh = new JSONErrorHandlerImpl(validation_results);
-			v.SetErrorHandler(eh);
-			
-			URL url=new URL(req.getParameter("uri"));
-			HTTPResponse get =URLFetchServiceFactory.getURLFetchService().fetch(new HTTPRequest(url, HTTPMethod.GET,followRedirects().setDeadline(60.0)));
-			String source=new String(get.getContent());
-			String[] args={source};
-			String[] names={url.toString()};
-			boolean hadErr = v.validate(args,names);
 
-			if (hadErr) {
-				resp.setStatus(409);
+			URL url = new URL(req.getParameter("uri"));
+			HTTPResponse get = URLFetchServiceFactory.getURLFetchService()
+					.fetch(
+							new HTTPRequest(url, HTTPMethod.GET,
+									followRedirects().setDeadline(60.0)));
+			String source = new String(get.getContent());
+
+			handleValidation(req, resp, source, url.toString(), opds_version);
+
+		} else {
+			resp.setStatus(400);
+		}
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("application/json");
+		String feed = req.getParameter("feed");
+		if (feed != null && !feed.equals("")) {
+			String opds_version = "1.0";
+			String ve = req.getParameter("opds_version");
+			if (ve != null && !ve.equals("")) {
+				opds_version = ve;
 			}
-			eh.close();
-			//resp.getOutputStream();
-			BufferedWriter w=new BufferedWriter(resp.getWriter());
-			w.append("{ \"results\": ");
-			w.append(validation_results.getBuffer());
-			w.append(", \"source\": ");
-			w.append(JSONObject.quote(source));
-			w.append("}");
 
-			
-			w.close();
-			
+			handleValidation(req, resp, feed, "direct", opds_version);
+
 		} else {
 			resp.setStatus(400);
 		}
